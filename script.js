49558165b7aff0be33d0e40e2c5750734bc27fb7
ulaali <ulaali513@yahoo.com>
2023-07-05 23:27:21 +0300

    to do list

--- script.js
+++ script.js
@@ -5,29 +5,16 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
 function additem() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("todoInput").value;
-  var t = document.createTextNode(inputValue);
+  var item = document.getElementById("todoInput").value;
+  var t = document.createTextNode(item);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (item === '') {
     alert("You must write something!");
   } else {
     document.getElementById("todoList").appendChild(li);
   }
   document.getElementById("todoInput").value = "";
 
-  // var span = document.createElement("SPAN");
-  // var txt = document.createTextNode("\u00D7");
-  // span.className = "close";
-  // span.appendChild(txt);
-  // li.appendChild(span);
-
-  // for (i = 0; i < close.length; i++) {
-  //   close[i].onclick = function() {
-  //     var div = this.parentElement;
-  //     div.style.display = "none";
-  //   }
-  // }
 }
