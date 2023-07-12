fc75ee4613775ad61ddb08d0821bb7bbf4738670
sanguine-dreams <sara0dhahir@gmail.com>
2023-07-12 17:46:35 +0300

    save state function

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,52 @@
 document.getElementById('add').addEventListener('click', function (event) {
-    event.preventDefault();
-    addItem();
+  event.preventDefault();
+  addItem();
 });
+
+// Function to add an item to the todo list
 function addItem() {
-    var todoInput = document.getElementById('todoInput');
-    var todoList = document.getElementById('todoList');
-  
-    if (todoInput.value !== '') {
+  var todoInput = document.getElementById('todoInput');
+  var todoList = document.getElementById('todoList');
+
+  if (todoInput.value !== '') {
       var listItem = document.createElement('li');
-  
+
       // Create a checkbox input element
       var checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.className = 'todo-checkbox';
-  
+
       // Create a label for the checkbox
       var label = document.createElement('label');
       label.textContent = todoInput.value;
-      label.className = 'checked-label'
-  
+      label.className = 'checked-label';
+
       // Append the checkbox and label to the list item
       listItem.appendChild(checkbox);
       listItem.appendChild(label);
-  
+
       // Append the list item to the todoList
       todoList.appendChild(listItem);
-  
+
       // Clear the input field
       todoInput.value = '';
-    }
+
+      saveState(); // Save the state after adding an item
   }
-  
- // Function to save the state
- function saveState() {
+}
+
+// Function to save the state
+function saveState() {
   var todos = [];
   var todoItems = document.querySelectorAll("#todoList li");
 
   // Iterate over the todo items and store their text content
   for (var i = 0; i < todoItems.length; i++) {
-    todos.push(todoItems[i].textContent);
+      todos.push(todoItems[i].textContent);
   }
 
   // Save the state in local storage
   localStorage.setItem("savedTodos", JSON.stringify(todos));
-  alert("State saved successfully!");
 }
 
 // Function to load the saved state
@@ -51,19 +54,15 @@ function loadState() {
   var savedTodos = localStorage.getItem("savedTodos");
 
   if (savedTodos) {
-    var todos = JSON.parse(savedTodos);
+      var todos = JSON.parse(savedTodos);
 
-    // Create and append list items for each saved todo
-    var todoList = document.getElementById("todoList");
-    for (var i = 0; i < todos.length; i++) {
-      var listItem = document.createElement("li");
-      listItem.textContent = todos[i];
-      todoList.appendChild(listItem);
-    }
-
-    alert("State loaded successfully!");
-  } else {
-    alert("No saved state found.");
+      // Create and append list items for each saved todo
+      var todoList = document.getElementById("todoList");
+      for (var i = 0; i < todos.length; i++) {
+          var listItem = document.createElement("li");
+          listItem.innerHTML = '<input type="checkbox" class="todo-checkbox"><label class="checked-label">' + todos[i] + '</label>';
+          todoList.appendChild(listItem);
+      }
   }
 }
 
